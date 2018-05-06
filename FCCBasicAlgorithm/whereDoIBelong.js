1ec978f9aa9fb49ffb4719263564d9a733617ebe
JQuinnie <jenn8866@gmail.com>
2018-05-05 23:08:42 -0600

    Add FCC basic algorithm exercises

---
+++ FCCBasicAlgorithm/whereDoIBelong.js
@@ -0,0 +1,8 @@
+// Return the lowest index at which a value (second argument) should be inserted into an array (first argument) once it has been sorted. The returned value should be a number.
+
+function getIndexToIns(arr, num) {
+  // Find my place in this sorted array.
+  return num;
+}
+
+getIndexToIns([40, 60], 50);
