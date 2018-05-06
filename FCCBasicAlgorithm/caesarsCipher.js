172d4dfd21ad5e8147b547d79e5da8e951bc4a6d
JQuinnie <jenn8866@gmail.com>
2018-05-06 09:56:57 -0600

    Update new algorithm challenge

---
+++ FCCBasicAlgorithm/caesarsCipher.js
@@ -0,0 +1,13 @@
+// Shift cipher
+// A common modern use is the ROT13 cipher, where the values of the letters are shifted by 13 places. Thus 'A' ↔ 'N', 'B' ↔ 'O' and so on.
+// Write a function which takes a ROT13 encoded string as input and returns a decoded string.
+// All letters will be uppercase. Do not transform any non-alphabetic character (i.e. spaces, punctuation), but do pass them on.
+
+function rot13(str) {
+  // LBH QVQ VG!
+
+  return str;
+}
+
+// Change the inputs below to test
+rot13("SERR PBQR PNZC");
