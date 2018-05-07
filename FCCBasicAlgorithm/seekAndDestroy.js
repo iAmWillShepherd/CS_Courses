3367ce39ad96d6af3437de14e381d74c4efe66c7
JQuinnie <jenn8866@gmail.com>
2018-05-07 17:40:32 -0600

    Update solution

--- FCCBasicAlgorithm/seekAndDestroy.js
+++ FCCBasicAlgorithm/seekAndDestroy.js
@@ -1,8 +1,18 @@
 // You will be provided with an initial array (the first argument in the destroyer function), followed by one or more arguments. Remove all elements from the initial array that are of the same value as these arguments.
 
-function destroyer(arr) {
+function destroyer(arr, ...args) {
   // Remove all the values
-  return arr;
+  console.log(arr);
+  console.log(args);
+
+  const seekArr = arr.filter(
+    // filter arr array for ->
+    // any element not present in args array since indexOf -1 mean element not found
+    element => args.indexOf(element) < 0
+  );
+  return seekArr;
 }
 
-destroyer([1, 2, 3, 1, 2, 3], 2, 3);
+const d = destroyer([1, 2, 3, 1, 2, 3], 2, 3);
+
+console.log(d);
