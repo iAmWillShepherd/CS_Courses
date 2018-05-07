7b2df9462e5f1334f66f7baebed020ccdbd45e16
JQuinnie <jenn8866@gmail.com>
2018-05-07 17:41:59 -0600

    Add comment to run es6 syntax

--- FCCBasicAlgorithm/seekAndDestroy.js
+++ FCCBasicAlgorithm/seekAndDestroy.js
@@ -1,5 +1,5 @@
 // You will be provided with an initial array (the first argument in the destroyer function), followed by one or more arguments. Remove all elements from the initial array that are of the same value as these arguments.
-
+/*jshint esversion: 6 */
 function destroyer(arr, ...args) {
   // Remove all the values
   console.log(arr);
