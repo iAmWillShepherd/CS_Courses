52dc274220446831eadfce03e4334dc240fcda07
JQuinnie <jenn8866@gmail.com>
2018-05-09 19:06:57 -0600

    Update sum of array function using recursion

--- Algorithms/sumFunction.js
+++ Algorithms/sumFunction.js
@@ -11,3 +11,17 @@ function sumArr(arr) {
 const numArr = [2, 4, 6];
 sumArr(numArr);
 console.log(sumArr(numArr));
+
+// make function above recursive
+function recurseSumArr(arr) {
+  // base case
+  if (arr.length < 1) {
+    return 0;
+  } else {
+    // total sum is the first number in list + the sum of the rest of the list
+    return arr[0] + recurseSumArr(arr.slice(1));
+  }
+}
+
+recurseSumArr(numArr);
+console.log(recurseSumArr(numArr));
