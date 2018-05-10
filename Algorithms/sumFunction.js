c8af845bcbc6e47b1f0490bfada399cfa8a9c490
JQuinnie <jenn8866@gmail.com>
2018-05-09 18:36:41 -0600

    Add simple sumArray function

--- Algorithms/sumFunction.js
+++ Algorithms/sumFunction.js
@@ -1,1 +1,13 @@
 // Given an array of numbers, add up all the numbers and return the total
+
+function sumArr(arr) {
+  let total = 0;
+  for (let i = 0; i < arr.length; i++) {
+    total += arr[i];
+  }
+  return total;
+}
+
+const numArr = [2, 4, 6];
+sumArr(numArr);
+console.log(sumArr(numArr));
