17a70b0ec022df4a31c5a666254d91c72137494d
JQuinnie <jenn8866@gmail.com>
2018-05-10 14:24:03 -0600

    Update with work in progress, using iterators and splice

--- FCCAlgorithm/diffTwoArrays.js
+++ FCCAlgorithm/diffTwoArrays.js
@@ -5,9 +5,36 @@
 /*jshint esversion: 6 */
 
 function diffArray(arr1, arr2) {
-  var newArr = [];
-  // Same, same; but different.
+  let newArr = arr1.concat(arr2);
+  console.log(newArr);
+  let diffArr = [];
+  for (let j = 0; j <= newArr.length + 1; j++) {
+    console.log(newArr);
+    console.log(newArr.length);
+    for (let i = 1; i < newArr.length; i++) {
+      let temp = newArr[0];
+      if (temp === newArr[i]) {
+        newArr.splice(0, 1);
+        console.log(newArr);
+        console.log(newArr.length);
+        newArr.splice(i - 1, 1);
+        console.log(newArr);
+        console.log(newArr.length);
+      }
+    }
+  }
   return newArr;
 }
 
-diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+//diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+//console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
+console.log(
+  diffArray(
+    ["andesite", "grass", "dirt", "pink wool", "dead shrub"],
+    ["diorite", "andesite", "grass", "dirt", "dead shrub"]
+  )
+);
+
+testArr = [1];
+testArr.splice(0, 1);
+console.log(testArr);
